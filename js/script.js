95998192f8f8394067b86005d2069681eb4dd3ef
ken_shimowada <ken.shimowada2@gmail.com>
2020-11-29 11:43:50 +0900

    201129
    top: ハンバーガー以外のレイアウト完成

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 // script.js
+
+$('.burger-btn').on('click',function(){//.burger-btnをクリックすると
+    $('.burger-btn').toggleClass('close');
+    //.btn_triggerにcloseクラスを付与(ボタンのアニメーション)
+    $('.開閉メニューのクラス').toggleClass('open');
+    //.開閉メニューのクラスにopenクラスを付与
+});
